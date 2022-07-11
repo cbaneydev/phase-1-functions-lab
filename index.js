9cc50a5db9758402625d50f33bcb288a57613680
Caeley Collins <cbaneydev@gmail.com>
2022-07-10 20:53:26 -0500

    finished project

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(a) {
+    let hq = 42
+    if(a > hq) {
+        return a - hq
+    } else if (a < hq) {
+        return hq - a
+    } else {
+        return "Please enter a starting block."
+    }
+}
+
+distanceFromHqInBlocks(50)
+
+function distanceFromHqInFeet(b) {
+    return distanceFromHqInBlocks(b) * 264
+}
+
+distanceFromHqInFeet(41)
+
+function distanceTraveledInFeet(a, b){
+    if(a > b) {
+        return (a - b) * 264
+    } else {
+        return (b - a) * 264
+    }
+}
+
+distanceTraveledInFeet(62, 60)
+
+function calculateFairPrice(a, b) {
+    let distance = distanceTraveledInFeet(a, b)
+    if(distance < 400){
+        return "It's free."
+    } else if (distance > 400 && distance < 2000){
+        return (distance - 400) * 0.02
+    } else if (distance > 2000 && distance < 2500){
+        return "It's twenty-five dollars."
+    } else if (distance > 2500){
+        return "We cannot travel that far."
+    }
+} 
+
+console.log(calculateFairPrice(34, 24))
